e5c934bdb22972b3d47dff293b28eb6044f2714b
avesyy <vlad556643@mail.ru>
2024-05-22 22:36:03 +0300

    Add files via upload

---
+++ lr.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+double CalcSum(int N);
+int laba1();
+int laba2();
+int laba3();
+int laba4();
+int laba5();
+int laba6();
+int laba7();
+int laba8();
+int laba9();
+int laba10();
+int laba11();
+int laba12();
